a56d128c2876be9b8eb2eff6ff6376ab2ef7721f
iisyos <kosuke1216a@yahoo.co.jp>
2022-02-22 00:18:50 +0900

    uniquekeyの勉強 #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   runApp(const MyApp());
 }
@@ -13,103 +13,141 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Boxes(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Box extends StatelessWidget {
+  const Box({Key? key,required this.color}) : super(key: key);
+  final  Color color;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return  Expanded(child: AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        color: color,
+      ) ,
+    )
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+class Boxes extends StatefulWidget {
+  const Boxes({Key? key}) : super(key: key);
+
+  @override
+  _BoxesState createState() => _BoxesState();
+}
+
+class Boxstful extends StatefulWidget {
+  const Boxstful({Key? key,required this.color}) : super(key: key);
+  final Color color;
+
+  @override
+  _BoxstfulState createState() => _BoxstfulState();
+}
+
+class _BoxstfulState extends State<Boxstful> {
+  @override
+  Widget build(BuildContext context) {
+    return   Expanded(child: AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        color: widget.color,
+      ) ,
+    )
+    );
+  }
+}
+
+class StatelessColorfulTile extends StatelessWidget {
+  final Color _color = UniqueColorGenerator.getColor();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: _color, child: Padding(padding: EdgeInsets.all(100.0)));
+  }
+}
+
+class UniqueColorGenerator {
+  static List _colorOptions = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.yellow,
+    Colors.purple,
+    Colors.orange,
+    Colors.indigo,
+    Colors.amber,
+    Colors.black,
+  ];
+  static Random _random = new Random();
+  static Color getColor() {
+    if (_colorOptions.length > 0)
+      return _colorOptions.removeAt(_random.nextInt(_colorOptions.length));
+    else
+      return Color.fromARGB(_random.nextInt(256), _random.nextInt(256),
+          _random.nextInt(256), _random.nextInt(256));
+  }
+}
+
+
+
+class StatefulColorfulTile extends StatefulWidget {
+  StatefulColorfulTile({Key? key}) : super(key: key);
+
+  @override
+  ColorfulTileState createState() => ColorfulTileState();
+}
+
+class ColorfulTileState extends State<StatefulColorfulTile> {
+  Color? _color;
+  @override
+  void initState() {
+    super.initState();
+    _color = UniqueColorGenerator.getColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: _color,
+        child: Padding(
+          padding: EdgeInsets.all(100.0),
+        ));
+  }
+}
+class _BoxesState extends State<Boxes> {
+  final List<Widget> _boxes=[
+    StatefulColorfulTile(key: UniqueKey()), // 変更！！
+    StatefulColorfulTile(key: UniqueKey()),
+  ];
+  void _swapTiles() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _boxes.insert(1, _boxes.removeAt(0));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Row(
+        children: _boxes,
+      ),floatingActionButton: FloatingActionButton(
+      onPressed: (){
+        _swapTiles();
+        // setState(() {
+        //   _boxes.shuffle();
+        // });
+      },
+    ),appBar: AppBar(
+
+    ),
     );
   }
 }
